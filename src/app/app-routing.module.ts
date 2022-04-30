11924dce58f1720f0d9c01ab34301e37e91ba39d
nikos liapis <nliapis1983@gmail.com>
2022-05-01 00:22:22 +0300

    Add Users module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: LayoutComponent,
+  children: [
+    {path: '', redirectTo: 'users', pathMatch: 'full'},
+    {path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule)},
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { LayoutModule } from './layout/layout.module';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    UsersModule
   ],
   providers: [],
-  bootstrap: [AppComponent, LayoutComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
