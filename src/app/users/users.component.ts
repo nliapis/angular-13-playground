89be71efe28dfe2a26caa06a34010d158b351e17
nikos liapis <nliapis1983@gmail.com>
2022-05-01 00:38:55 +0300

    Fetch and render Users

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface User {
+  id: number;
+  givenName: string;
+  familyName: string;
+}
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  users: User[] = [];
 
   ngOnInit(): void {
+    fetch('http://localhost:3001/Users')
+      .then(response => response.json())
+      .then(data => this.users = data);
   }
-
 }
